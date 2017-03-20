7499213281946295f3a3aaffb050b76d54bc2014
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-20 12:29:06 -0400

    clean up code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
 // Import the model (burger.js) to use its database functions.
-// var burger = require('../models/burger.js');
+
 var models = require('../models');
 
 router.get('/', function(req,res){
-	// burger.selectAll(function(burger_data){
-	// 	console.log(burger_data);
-	// 	res.render('index',{burger_data});
 	res.redirect('/burger');
 	});
-// })
 
 router.get('/burger', function (req, res) {
   models.burger.findAll({}).then(function (data) {
@@ -20,8 +16,6 @@ router.get('/burger', function (req, res) {
 });
 
 router.post('/burgers/create', function(req,res){
-	// burger.insertOne(req.body.burger_name, function(result){
-	// 	res.redirect('/');
   models.burger.create({burger_name: req.body.burger_name, devoured: req.body.devoured}).then(function() {
     res.redirect('/burger');
 	});
@@ -38,13 +32,6 @@ router.delete('/burger/delete/:id', function (req, res) {
     res.redirect('/burger');
   });
 });
-
-// router.put('/burgers/update', function(req,res){
-// 	burger.updateOne(req.body.burger_id, function(result){
-// 		console.log(result);
-// 		res.redirect('/');
-// 	});
-// });
 
 // Export routes for server.js to use.
 module.exports = router;
