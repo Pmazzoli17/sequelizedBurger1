13450e5689d57e2aa3986b4f884f5fe0ea93c1a7
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-23 09:31:18 -0400

    delete unnecessary files

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cheese Burger', FALSE, CURRENT_TIMESTAMP);
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Breakfast Burger', FALSE, CURRENT_TIMESTAMP);
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Hamburger', FALSE, CURRENT_TIMESTAMP);
