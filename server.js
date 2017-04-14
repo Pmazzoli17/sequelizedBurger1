8c3c384efbf0b2d3346988ed7921c29c3de2286d
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-04-14 09:20:29 -0400

    heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs({ defaultLayout: 'main'
