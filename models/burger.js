c0fdac051f4f76cdee24e2fe4ea09affca1590f1
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-05-23 13:41:04 -0400

    style changes

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,71 @@
-var Sequelize = require('sequelize');
+// Burger models
+
+// The burger has a burger_name attribute of type DataTypes.String
+// and a devoured attribute that is false by default
 
 module.exports = function(sequelize, DataTypes) {
-  var burger = sequelize.define('burger', 
-  {
-    burger_name: DataTypes.STRING,
+  var Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     devoured: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
-    },
-    date: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: Date.now()
     }
-   
   }, {
-    timestamps:false,
     classMethods: {
       associate: function(models) {
+        Burger.hasOne(models.Customer);
       }
     }
   });
-  return burger;
+  return Burger;
 };
+
+
+
+
+// var Sequelize = require('sequelize');
+
+// module.exports = function(sequelize, DataTypes) {
+//   var burger = sequelize.define('burger', {
+//     burger_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     devoured: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false
+//     }
+//   }, {
+//     classMethods: {
+//       associate: function(models) {
+//         burger.hasOne(models.Customer);
+//       }
+//     }
+//   });
+//   return burger;
+// };
+
+//     burger_name: DataTypes.STRING,
+//     devoured: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false
+//     },
+//     date: {
+//       allowNull: false,
+//       type: Sequelize.DATE,
+//       defaultValue: Date.now()
+//     }
+   
+//   }, {
+//     timestamps:false,
+//     classMethods: {
+//       associate: function(models) {
+//       }
+//     }
+//   });
+//   return burger;
+// };
 
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 
+// bring in the models
+var db = require("./models");
+
 // Sets up the Express App
 var app = express();
 
@@ -15,6 +18,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(methodOverride('_method'));
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
@@ -23,6 +27,10 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
+app.use("/update", routes);
+app.use("/create", routes);
 
 // Starts the server to begin listening
+db.sequelize.sync().then(function() {
 app.listen(port);
+});
